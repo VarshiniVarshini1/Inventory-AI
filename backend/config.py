80ced8d4b086f2f51f5b1a5f5a53a68e304d9c11
Susmitha Varshini <varshini7027@gmail.com>
2025-11-15 17:07:47 +0530

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+DATABASE_URL = "postgresql://postgres:password@localhost/inventorydb"
+JWT_SECRET = "your_jwt_secret"
+JWT_ALGORITHM = "HS256"
+
+EMAIL_USER = "yourmail@gmail.com"
+EMAIL_PASS = "yourpassword"
+
+WHATSAPP_API = "your_whatsapp_api_key"
